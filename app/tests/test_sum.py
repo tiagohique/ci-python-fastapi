bece35b128cf390f8974a75233214b5fe7b0c11e
Tiago  Silva <tiagosilva@MacBook-Pro-de-Tiago.local>
2024-07-24 18:28:08 -0300

    Altercao de esteira codigo errado

--- app/tests/test_sum.py
+++ app/tests/test_sum.py
@@ -10,5 +10,5 @@ def test_read_root():
 
 def test_calculate_sum():
     response = client.post("/sum", json={"value1": 10.5, "value2": 5.5})
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert response.json() == {"sum": 16.0}

fbbceee79af7d09371ee48050bf0c6e7339a66c8
Tiago  Silva <tiagosilva@MacBook-Pro-de-Tiago.local>
2024-07-24 18:37:22 -0300

    Incluindo cenario de erro

--- app/tests/test_sum.py
+++ app/tests/test_sum.py
@@ -10,5 +10,20 @@ def test_read_root():
 
 def test_calculate_sum():
     response = client.post("/sum", json={"value1": 10.5, "value2": 5.5})
-    assert response.status_code == 201
+    assert response.status_code == 200
     assert response.json() == {"sum": 16.0}
+
+
+def test_invalid_input():
+    response = client.post("/sum", json={"value1": "rewrer5.5", "value2": 5.5})
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "value1"],
+                "msg": "Input should be a valid number, unable to parse string as a number",
+                "type": "float_parsing",
+                "input": "rewrer5.5"
+            }
+        ]
+    }

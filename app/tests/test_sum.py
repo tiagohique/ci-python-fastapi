d4d8dcceb821e06c8461b87cd4da0d0b34cf7b6e
Tiago  Silva <tiagosilva@MacBook-Pro-de-Tiago.local>
2024-08-22 19:17:02 -0300

    Ajustes no teste

--- app/tests/test_sum.py
+++ app/tests/test_sum.py
@@ -7,7 +7,7 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the sum API"}
-'''
+
 def test_calculate_sum():
     response = client.post("/sum", json={"value1": 10.5, "value2": 5.5})
     assert response.status_code == 200
@@ -58,9 +58,3 @@ def test_invalid_input_no_body():
     response = client.post("/sum")
     assert response.status_code == 422
     assert "detail" in response.json()
-
-
-
-
-
-'''

6b59d58740f3c1cea9858690385d210aa130510f
Tiago  Silva <tiagosilva@MacBook-Pro-de-Tiago.local>
2024-08-28 19:09:21 -0300

    coloando no appruner

--- iac/iam.tf
+++ iac/iam.tf
@@ -14,6 +14,31 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "app-runner-role" {
+  name = "app-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
+
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "ecr-role" {
   name = "ecr-role"
 
@@ -43,26 +68,41 @@ resource "aws_iam_role" "ecr-role" {
   inline_policy {
     name = "ecr-app-permission"
 
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Sid    = "Statement1"
-          Action = [
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:PutImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:GetAuthorizationToken"
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
+policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [
+    {
+      Sid = "Statement1",
+      Action = "apprunner:*",
+      Effect = "Allow",
+      Resource = "*"
+    },
+    {
+      Sid     = "Statement2",
+      Action  = [
+        "iam:PassRole",
+        "iam:CreateServiceLinkedRole"
+      ],
+      Effect  = "Allow",
+      Resource = "*"
+    },
+    {
+      Sid = "Statement3",
+      Action = [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:PutImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload",
+        "ecr:GetAuthorizationToken"
+      ],
+      Effect = "Allow",
+      Resource = "*"
+    }
+  ]
+})
   }
 
   tags = {

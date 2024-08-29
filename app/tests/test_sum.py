248eaa83c6d3e0ec94ebdfd177d0ea2cb6ed6607
tiagohique <tiago.hique@gmail.com>
2024-08-29 17:02:10 -0300

    Versao Nova...

--- app/tests/test_sum.py
+++ app/tests/test_sum.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the sum API - Tiago v1.0"}
+    assert response.json() == {"message": "Welcome to the sum API - Tiago v2.5"}
 
 def test_calculate_sum():
     response = client.post("/sum", json={"value1": 10.5, "value2": 5.5})
